91b54c456a4b26f9199b8019f4f9da51677aaeb6
Luiz T. F. Eleno <luizeleno@gmail.com>
2021-08-08 01:08:59 -0300

    Build site at 2021-08-08 04:08:59 UTC

--- cron-script.bash
+++ cron-script.bash
@@ -7,6 +7,9 @@
 # - rodar `python3 raspa_eel.py` para gerar os arquivos json (um por curso)
 # - rodar `python3 consolidate-disciplinas-database.py` para gerar os arquivos `disciplinas.json`, `disciplinas.yml` e `cursos.yml`
 
+echo '#' `date`
+
+
 cd /home/eleno/Dropbox/Lorena/Website/pyjupiter/_python
 
 python3 raspa_eel.py
