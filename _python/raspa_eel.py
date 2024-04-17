7ff43d988931a668d2d64f9732b15333d941a007
luizeleno <luizeleno@gmail.com>
2024-04-17 10:28:43 -0300

    Build site at 2024-04-17 13:28:43 UTC

--- _python/raspa_eel.py
+++ _python/raspa_eel.py
@@ -6,12 +6,12 @@ EB = 'EB', '152', '0'
 EF = 'EF', '301', '0'
 EM = 'EM', '202', '0'
 EP = 'EP', '352', '4'
-EQD = 'EQD', '052', '1'
+EQD = 'EQD', '052', '0'
 EQN = 'EQN', '052', '4'
 
 for curso, codigo, hab in [EF, EM, EA, EB, EP, EQD, EQN]:
     URL = f'https://uspdigital.usp.br/jupiterweb/listarGradeCurricular?codcg=88&codcur=88{codigo}&codhab={hab}&tipo=N'
-    print(curso)
+    print(f"\n#######################\n{curso}#######################\n")
 
     dados = rc.scrape_curso(URL)
 
